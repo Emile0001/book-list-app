be653298411344d14bbb2bcd3b1bdb259ba91c29
Emile0001 <boteseh@gmail.com>
2024-07-27 12:01:17 +0200

    consume book api

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../book';
 import { CommonModule } from '@angular/common';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -9,11 +10,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss',
 })
-export class BookListComponent {
-  // book: Book = { id: 1, author: 'Mielie Pitt', title: 'Book one' };
+export class BookListComponent implements OnInit {
   books: Book[] = [
-    { id: 1, title: 'coding', author: 'OG' },
-    { id: 2, title: '4', author: 'Mielie' },
-    { id: 3, title: 'life', author: 'Pitt' },
+    // { id: 1, title: 'coding', author: 'OG' },
+    // { id: 2, title: '4', author: 'Mielie' },
+    // { id: 3, title: 'life', author: 'Pitt' },
   ];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.bookService
+      .getBooks()
+      .subscribe((booksFromApi) => (this.books = booksFromApi));
+  }
 }
---
+++ src/app/book.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from './book';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  private apiUrl = 'https://localhost:7253';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>(this.apiUrl);
+  }
+}
